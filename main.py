f144494a2354c8d0c1eacd5f1ab66989b57235b7
kuisa <32757027+kuisa@users.noreply.github.com>
2026-05-07 22:33:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -436,8 +436,13 @@ class BytenutRenewal:
                 try:
                     # 登录
                     sb.uc_open_with_reconnect(URL_LOGIN_PANEL, reconnect_time=5)
-                        if not self.wait_turnstile(sb):
-                        return False, ""
+                    time.sleep(5)
+                    try:
+                        sb.uc_gui_click_captcha()
+                        sb.uc_gui_handle_captcha()
+                    except:
+                        pass
+                    time.sleep(5)
                     sb.wait_for_element_visible('input[placeholder="Username"]', timeout=25)
                     sb.type('input[placeholder="Username"]', user)
                     sb.type('input[placeholder="Password"]', pwd)
